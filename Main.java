3fdcf8b65a1bbe5af14596caf4e104ecdc8df192
Gabriel Farias Nunes <gabrielfariasdp@gmail.com>
2024-10-05 19:40:13 -0300

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-
-import server.ChatServer;
-public class Main {
-	private static final int SERVER_LISTEN_PORT = 8089;
-	public static void main(String[] args) {
-		ChatServer.create(SERVER_LISTEN_PORT).start();
-    }
-}
